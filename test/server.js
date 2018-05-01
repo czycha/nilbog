7936d89f9ad4b65ff0c279492fad9b19687d17af
James Anthony Bruno <j.bruno.che@gmail.com>
2018-05-01 10:59:22 -0500

    Test server: allow custom port

--- test/server.js
+++ test/server.js
@@ -2,7 +2,13 @@ const path = require('path')
 const express = require('express')
 
 const app = express()
-
 app.use(express.static(path.join(__dirname, 'browser')))
 
-app.listen(4444)
+let port = 4444
+const index = Math.max(process.argv.indexOf('--port'), process.argv.indexOf('-p'))
+if(index !== -1) {
+	port = +process.argv[index + 1] || port
+}
+console.log('Listening on port ' + port)
+
+app.listen(port)
