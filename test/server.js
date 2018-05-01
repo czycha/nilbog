85512908cfbd59044194710d0d2c30820e670759
James Anthony Bruno <j.bruno.che@gmail.com>
2018-05-01 11:05:00 -0500

    Server start displays path

--- test/server.js
+++ test/server.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const express = require('express')
+const chalk = require('chalk')
 
 const app = express()
 app.use(express.static(path.join(__dirname, 'browser')))
@@ -9,6 +10,6 @@ const index = Math.max(process.argv.indexOf('--port'), process.argv.indexOf('-p'
 if(index !== -1) {
 	port = +process.argv[index + 1] || port
 }
-console.log('Listening on port ' + port)
 
 app.listen(port)
+console.log(`Server started at ${chalk.green(`http://localhost:${port}/`)}`)
